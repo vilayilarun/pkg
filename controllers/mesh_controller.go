7c254ba0029a140f73cdae5be2a64efb976dd481
root <root@wzd419.workz.com>
2023-12-08 18:51:24 +0400

    logger add

--- controllers/mesh_controller.go
+++ controllers/mesh_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"context"
 	corev1 "k8s.io/api/core/v1"
+	logr "github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
