e2fc0bf2542fad6d4c9bbbe9c954f82cbc7c3fc1
root <root@wzd419.workz.com>
2023-12-11 12:56:39 +0400

    logger add

--- main.go
+++ main.go
@@ -27,8 +27,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	// ctrl "sigs.k8s.io/controller-runtime"
-	ctrl "sigs.k8s.io/controller-runtime@v0.16.3"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
